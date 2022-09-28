7ef4cc13a7bbc0b5198986aa5323f614868dd0d5
Kyle Trout <kt.trout@gmail.com>
2022-09-28 15:05:14 -0400

    Add mutation rules

--- index.js
+++ index.js
@@ -19,12 +19,26 @@ const TEST_INPUT = [
   [0, 0, 0, 0, 0]
 ];
 
-// const Age = {
-//   EMPTY: 0,
-//   NEWBORN: 1,
-//   ADULT: 2,
-//   SENIOR: 3
-// };
+const Age = {
+  EMPTY: 0,
+  NEWBORN: 1,
+  ADULT: 2,
+  SENIOR: 3
+};
+
+const mutationRules = {
+  [Age.EMPTY]: neighbors => {
+    const adultNeighbors = neighbors.filter(neighbor => neighbor === Age.ADULT).length;
+    return adultNeighbors === 2 ? Age.NEWBORN : Age.EMPTY;
+  },
+  [Age.NEWBORN]: neighbors => {
+    return neighbors.length >= 5 || neighbors.length <= 1 ? Age.EMPTY : Age.ADULT;
+  },
+  [Age.ADULT]: neighbors => {
+    return neighbors.length === 0 || neighbors.length >= 3 ? Age.EMPTY: Age.SENIOR;
+  },
+  [Age.SENIOR]: () => Age.EMPTY
+};
 
 nextGeneration(TEST_INPUT);
 
@@ -35,20 +49,23 @@ nextGeneration(TEST_INPUT);
  * @return {undefined}
  */
 function nextGeneration(grid) {
-  if (!Array.isArray(grid) || !Array.isArray(grid[0])) {
-    console.error('Invalid format for grid.');
+  const errMsg = validateGrid(grid);
+  if (errMsg) {
+    console.error(errMsg);
     return;
   }
 
-  const rows = grid.length;
-  const columns = grid[0].length;
-  if (rows !== columns) {
-    console.error('Dimension mismatch for grid.');
-    return;
-  }
+  const neighbors = getNeighbors(0, 2, grid);
+  const newValue = mutationRules[1](neighbors);
+  console.log('newValue', newValue);
 
-  const neighbors = getNeighbors(0, 0, grid);
-  console.log('neighbors', neighbors);
+  // grid.forEach((row, i) => {
+  //   row.forEach((column, j) => {
+  //     // For each cell, get neighbors
+  //     const neighbors = getNeighbors(i, j, grid);
+  //     console.log('neighbors', neighbors);
+  //   });
+  // });
 }
 
 /**
@@ -68,13 +85,31 @@ function getNeighbors(rowIndex, columnIndex, grid) {
         continue;
       }
 
-      // If value is within grid, it is a valid neighbor
+      // If value is within grid and not empty, it is a valid neighbor
       const row = grid[i];
-      if (row !== undefined && row[j] !== undefined) {
+      if (row !== undefined && row[j]) {
         neighbors.push(row[j]);
       }
     }
   }
 
   return neighbors;
+}
+
+/**
+ * Validate basic requirements of grid.
+ *
+ * @param  {number[][]}  grid    Grid of cells
+ * @return {string | undefined}  Array of adjacent cells
+ */
+function validateGrid(grid) {
+  if (!Array.isArray(grid) || !Array.isArray(grid[0])) {
+    return 'Invalid format for grid.';
+  }
+
+  const rows = grid.length;
+  const columns = grid[0].length;
+  if (rows !== columns) {
+    return 'Dimension mismatch for grid.';
+  }
 }
